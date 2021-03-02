b5b4c902186d3a8a3f15edc6591e3af2db3d4c6b
Simon Panek <paneks19@gmail.com>
2021-03-02 15:26:02 -0800

    move cors above app

--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
 'use strict';
 
 const express = require('express');
-const app = express();
 const cors = require('cors');
-app.use(cors()); //moved from below
+const app = express();
 const morgan = require('morgan');
 require('./models/student');
 const mongoose = require('mongoose');
@@ -17,7 +16,7 @@ const apiRoutes = require('./routes/apiRoutes.js');
 const logger = require('./middleware/logger.js');
 
 //App middleware
-//app.use(cors()); //Simon moved up 3/2/21 per Nathan suggestion
+app.use(cors()); 
 app.use(morgan('dev'));
 
 app.use(express.json());
